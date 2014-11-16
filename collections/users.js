2e099438251b4bb5d7076f9fdbcd807e52a4fd7d
Alexander Bjerkan <alb@knowit.no>
2014-11-16 14:31:09 +0100

    Added a simple schema for user collection

---
+++ collections/users.js
@@ -0,0 +1,65 @@
+
+Users = new SimpleSchema({
+    username: {
+        type: String,
+        regEx: /^[a-z0-9A-Z_]{3,15}$/
+    },
+    emails: {
+        type: [Object],
+        optional: true
+        // this must be optional if you also use other login services like facebook,
+        // but if you use only accounts-password, then it can be required
+    },
+    "emails.$.address": {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email
+    },
+    "emails.$.verified": {
+        type: Boolean
+    },
+    createdAt: {
+        type: Date
+    },
+    profile: {
+        type: Object,
+        optional: true,
+        blackbox: true
+    },
+    services: {
+        type: Object,
+        optional: true,
+        blackbox: true
+    },
+    isAdmin: {
+        type: Boolean,
+        defaultValue: false
+    },
+    score: {
+        type: Number,
+        defaultValue: 0
+    },
+    solved: {
+        type: Number,
+        defaultValue: 0
+    },
+    linje: {
+        type: String,
+        allowedValues: ['Online', 'Abakus'],
+        defaultValue: 'Online'
+    }
+});
+
+Meteor.users.attachSchema(Users);
+
+
+Meteor.users.allow({
+    insert: isAdminById,
+    update: isAdminById,
+    remove: isAdminById
+});
+
+if (Meteor.isServer) {
+    Meteor.users.after.remove(function (userId, user) {
+        Problems.update({}, {$pull: {answers: {userId: user._id}}}, {multi: true});
+    });
+}
--- server/accounts.js
+++ server/accounts.js
@@ -1,11 +1,3 @@
-Accounts.onCreateUser(function (options, user) {
-    user.score = 0
-    user.solved = 0
-    user.isAdmin = false
-    user.linje = 0
-    return user;
-});
-
 Accounts.validateNewUser(function (user) {
     if (!getSetting('userSignup', true)) {
         throw new Meteor.Error(403, "User sign up is currently disabled.");
