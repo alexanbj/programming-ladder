cbadf4a2020158de2b8e5e04172ea5cda7cf981b
Alexander Bjerkan <alb@knowit.no>
2015-03-27 07:37:58 +0100

    Moved spacebars helpers from CS to JS

---
+++ client/helpers/spacebars.js
@@ -0,0 +1,19 @@
+UI.registerHelper('isAdmin', function() {
+    return isAdmin(Meteor.user());
+});
+
+UI.registerHelper('formatDate', function(datetime, format) {
+    if (datetime) {
+        return moment(datetime).format(format);
+    } else {
+        return "";
+    }
+});
+
+UI.registerHelper('isDateBeforeNow', function(date) {
+    return date > Date.now();
+});
+
+UI.registerHelper('isDateAfterNow', function(date) {
+    return date < Date.now();
+});
