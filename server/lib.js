56515b6f3673bee760848a88e3c3feb3bd32496f
Alexander Bjerkan <alb@knowit.no>
2015-03-27 08:38:57 +0100

    getSetting was duplicated on the server

--- server/lib.js
+++ server/lib.js
@@ -1,13 +1,3 @@
-getSetting = function(setting, defaultValue) {
-    var settings = Settings.find().fetch()[0];
-    if (settings && (typeof settings[setting] !== 'undefined')) {
-        return settings[setting];
-    } else {
-        return typeof defaultValue === 'undefined' ? '' : defaultValue;
-    }
-};
-
-
 countSolved = function(answers) {
     return _.size(_.where(answers, {solved: true}));
 };
