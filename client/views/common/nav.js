5a3610d90d9d6d5f8eb27cb7fe28988be94fd8c6
Alexander Bjerkan <alb@knowit.no>
2015-04-20 10:37:52 +0200

    Fixed admin dropdown for desktop and mobile

--- client/views/common/nav.js
+++ client/views/common/nav.js
@@ -1,7 +1,17 @@
-Template.nav.rendered = function () {
-    this.$(".dropdown-button").dropdown({hover: false});
+Template.nav.onRendered (function () {
+    var self = this;
+
+    this.autorun(function() { // Reactively update on the admin property
+        if (isAdmin(Meteor.user())) {
+            Meteor.defer(function() { // Defer execution until the browser has had the chance to render dropdown
+                self.$(".dropdown-button").dropdown({hover: false});
+                self.$('.collapsible').collapsible();
+            });
+        }
+    });
+
     this.$(".button-collapse").sideNav();
-};
+});
 
 
 Template.nav.events({
